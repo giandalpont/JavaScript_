317b90fdaa06f8c5c17764ca3b5d1608cb9715f5
Gian Carlos Dal Pont <giancarlos@MacBook-Pro-de-Gian.local>
2018-07-26 14:41:40 -0300

    challenge-02

---
+++ Aula-02/challenge-02.js
@@ -0,0 +1,104 @@
+/* 
+    Desafio da semana #2
+
+    Nesse exercício, você está livre para escolher os nomes para suas variáveis e funções! :smile:
+*/
+// Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
+function soma (x, y){
+    return x + y
+}
+console.log(soma(3 ,4))
+
+// Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5` ao resultado retornado da função.
+var iSoma = soma(2,3)
+console.log(iSoma)
+
+// Qual o valor atualizado dessa variável?
+console.log(iSoma)
+5
+
+
+// Declare uma nova variável, sem valor.
+var sValor
+console.log(sValor)
+
+/*
+Crie uma função que adicione um valor à variável criada acima, e retorne a string:
+    O valor da variável agora é VALOR.
+Onde VALOR é o novo valor da variável.
+*/
+function addValor (){
+    sValor = 9 
+    return `O valor da variável é ${sValor}`
+} 
+console.log(addValor())
+
+
+// Invoque a função criada acima.
+addValor()
+
+
+// Qual o retorno da função? (Use comentários de bloco).
+/* O valor da variável é 9 */
+
+/*
+Crie uma função com as seguintes características:
+1. A função deve receber 3 argumentos;
+2. Se qualquer um dos três argumentos não estiverem preenchidos, a função deve retornar a string:
+    Preencha todos os valores corretamente!
+3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
+*/
+function nNova(x, y, z){
+    if(x === undefined || y === undefined || z === undefined){
+        return 'Preencha todos os valores corretamente!'
+    } else{
+        return (x * y * z) + 2
+    } 
+}
+console.log(nNova(2, 3, 4))
+
+
+// Invoque a função criada acima, passando só dois números como argumento.
+nNova(2, 2)
+console.log(nNova(2, 2))
+
+// Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+// Preencha todos os valores corretamente!
+
+// Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
+nNova(2, 2, 2)
+console.log(nNova(2, 2, 2))
+
+// Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+// 10
+
+/*
+Crie uma função com as seguintes características:
+1. A função deve receber 3 argumentos.
+2. Se somente um argumento for passado, retorne o valor do argumento.
+3. Se dois argumentos forem passados, retorne a soma dos dois argumentos.
+4. Se todos os argumentos forem passados, retorne a soma do primeiro com o segundo, e o resultado, dividido pelo terceiro.
+5. Se nenhum argumento for passado, retorne o valor booleano `false`.
+6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
+*/
+function bigG(a ,b ,c){
+    if(a != undefined && b === undefined && c === undefined ){
+        return a 
+    }else if (a != undefined && b != undefined && c === undefined){
+        return a + b
+    } else if(a != undefined && b != undefined && c != undefined){
+        return (a + b) /c
+    }else if(a === undefined && b === undefined && c === undefined){
+        return false
+    }else{
+        return null
+    }
+}
+console.log(bigG(2,2,2))
+
+
+
+// Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
+console.log(bigG(2,)) // 2
+console.log(bigG(2,2)) //  4
+console.log(bigG(2,2,2)) // 2
