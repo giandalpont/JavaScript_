5d074e712dad0c88bf071e08bc07d3db581f22ed
Gian Carlos Dal Pont <35267440+giandalpont@users.noreply.github.com>
2018-08-06 12:34:54 -0300

    challenge-10

--- Aula-10/challenge-10.js
+++ Aula-10/challenge-10.js
@@ -147,18 +147,76 @@
     - O segundo, a função de soma, passando os dois operandos.
     - Se "sum" for "false", mostrar no console a mensagem de erro.
     */
-    // ?
+    if( sum ){
+        number1 = 4;
+        number2 = 3;
+        console.log( showOperationMessage( operationSignal, number1, number2 ), sum( number1, number2 ) );
+    } 
+    else{
+        console.log( showErrorMessage( operationSignal ) );
+    }
 
     /*
     Repita desde o "PASSO 2" com as operações de subtração, multiplicação,
     divisão e resto. Crie variáveis com os nomes "subtraction",
     "multiplication", "division" e "mod".
     */
-    // ?
+    operationSignal = '-';
+    var subtraction = calculator( operationSignal );
+    if( subtraction ){
+        number1 = 20;
+        number2 = 9;
+        console.log( showOperationMessage( operationSignal, number1, number2 ), subtraction( number1, number2 ) );
+    }
+    else{
+        console.log( showErrorMessage( operationSignal ) )
+    }
+
+    operationSignal = '*';
+    var multiplication = calculator( operationSignal );
+    if( multiplication ){
+        number1 = 3;
+        number2 = 7;
+        console.log( showOperationMessage(operationSignal, number1, number2), multiplication( number1, number2 ));
+    } 
+    else{
+        console.log( showErrorMessage( operationSignal ) )
+    }
+
+    operationSignal = '/';
+    var dividion = calculator( operationSignal );
+    if ( dividion ){
+        number1 = 100;
+        number2 = 5;
+        console.log( showOperationMessage( operationSignal, number1, number2 ), dividion( number1, number2 ) );
+    }
+    else{
+        console.log( showErrorMessage( operationSignal ) );
+    }
+
+    operationSignal = '%';
+    var mod = calculator( operationSignal );
+    if( mod ){
+        number1 = 332;
+        number2 = 49;
+        console.log( showOperationMessage( operationSignal, number1, number2 ), mod( number1, number2 ) ); 
+    }
+    else{
+        console.log( showErrorMessage( operationSignal ) )
+    }
 
     /*
     Repita o PASSO 2 novamente, mas passando um operador inválido, para ver se
     a mensagem de erro será mostrada no console.
     */
-    // ?
+    operationSignal = 'js';
+    var invalid = calculator( operationSignal );
+    if( invalid ){
+        number1 = 20;
+        number2 = 9;
+        console.log( showOperationMessage( operationSignal, number1, number2 ), invalid( number1, number2 ) );
+    }
+    else{
+        console.log( showErrorMessage( operationSignal ) )
+    }
 })();
