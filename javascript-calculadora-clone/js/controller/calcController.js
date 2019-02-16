5dba38837b95f3b1894319ab65fca2ec23b8ed9f
Gian Carlos Dal Pont <giandalpont@gmail.com>
2019-02-16 09:01:09 -0200

    manipulando o DOM

--- javascript-calculadora-clone/js/controller/calcController.js
+++ javascript-calculadora-clone/js/controller/calcController.js
@@ -2,8 +2,18 @@ class  CalcController{
     constructor(){
 
         this._displayCalc = 0;
-        this._dataAtual; 
-        
+        this._dataAtual;
+        this.initialize();
+    }
+    initialize(){
+        // Select element display
+        let displayCalcEl = document.querySelector("#display")
+        let dataEl = document.querySelector("#data")
+        let timeEl = document. querySelector("#hora")
+
+        displayCalcEl.innerHTML = "112"
+        dataEl.innerHTML = '16/02/2019'
+        timeEl.innerHTML = '00:00'
     }
     get displayCalc(){
         return this._displayCalc;
